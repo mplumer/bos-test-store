2a5a36b6d730dd67d6421c868ebb26ebddf4d251
Max Plumer <maxplumer12@gmail.com>
2021-07-30 20:01:07 -0400

    bos product info loaded

---
+++ src/icons/logo.jsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+function Logo(props) {
+  return (
+    <img src="https://images.ctfassets.net/ueb5d22hcbpm/4VHLvaV2qiHHKQlyGFN9jq/9893ea8807ebf13edb11f66e46e6f308/BoS-logo.png"/>
+  )
+}
+
+export default Logo
