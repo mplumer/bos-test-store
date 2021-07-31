6cdd826a0324b8847baa9a802a2b71fcf41139b1
Max Plumer <maxplumer12@gmail.com>
2021-07-30 21:21:19 -0400

    logo

--- src/icons/logo.jsx
+++ src/icons/logo.jsx
@@ -2,7 +2,7 @@ import * as React from "react"
 
 function Logo(props) {
   return (
-    <img src="https://images.ctfassets.net/ueb5d22hcbpm/4VHLvaV2qiHHKQlyGFN9jq/9893ea8807ebf13edb11f66e46e6f308/BoS-logo.png"/>
+    <img src="https://images.ctfassets.net/ueb5d22hcbpm/PuGNxwmNHWzh1xN7jG7wa/0bec572eef1835a6ee6a2a6b735b51ba/LOGO-FINALs-02.png?h=250"/>
   )
 }
 
